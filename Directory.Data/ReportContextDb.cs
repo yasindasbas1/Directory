afddff5673bdadc47aba6c1e6c6be76734fef7f0
yasindasbas1 <yasin.dasbas1@gmail.com>
2023-05-18 22:43:03 +0300

    Rapor servisi için DbContext nesnesi oluşturuldu.

---
+++ Directory.Data/ReportContextDb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Directory.Data.Entities;
+
+namespace Directory.Data
+{
+    public class ReportContextDb : DbContext
+    {
+        public ReportContextDb(DbContextOptions<ReportContextDb> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ReportContextDb).Assembly);
+        }
+
+        public virtual DbSet<Report> Reports { get; set; } = null!;
+        public virtual DbSet<ReportDetail> ReportDetails { get; set; } = null!;
+        
+    }
+}
