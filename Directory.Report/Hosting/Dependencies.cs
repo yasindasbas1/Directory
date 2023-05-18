81495991fc4b8d054f1854cbe10adc72fbae9b3a
yasindasbas1 <yasin.dasbas1@gmail.com>
2023-05-18 23:14:40 +0300

    Rapor servisi oluşturuldu.

---
+++ Directory.Report/Hosting/Dependencies.cs
@@ -0,0 +1,25 @@
+﻿using Directory.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Directory.Report.Hosting
+{
+    public static class Dependencies
+    {
+        public static void ConfigureDependencies(this IServiceCollection services, IWebHostEnvironment env, IConfiguration configuration)
+        {
+            services.AddDbContext<ReportContextDb>(builder =>
+            {
+                builder.UseSqlServer(configuration.GetConnectionString("ConnectionString"));
+                builder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+
+                if (!env.IsDevelopment())
+                    return;
+
+                builder.EnableSensitiveDataLogging();
+                builder.EnableDetailedErrors();
+
+            });
+        }
+    }
+    
+}
---
+++ Directory.Report/Hosting/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace Directory.Report.Hosting
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+        private IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .ConfigureModelValidationOptions();
+
+            services.ConfigureDependencies(Environment, Configuration);
+            services.ConfigureSwagger();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.ConfigureCorsOrigin();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.ConfigureSwagger(env);
+        }
+    }
+}
