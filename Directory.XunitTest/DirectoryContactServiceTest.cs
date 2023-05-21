9dc85e8341e1babbed1c779768701a7ba7c8989b
yasindasbas1 <yasin.dasbas1@gmail.com>
2023-05-21 23:46:27 +0300

    Contaxt Servici için dönen veri tipi, ekleme işle mi dönüş sonucu ve dönen veri boş mu kontrolleri için testler yazıldı.

--- Directory.XunitTest/DirectoryContactServiceTest.cs
+++ Directory.XunitTest/DirectoryContactServiceTest.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using Directory.Contact.Models;
 using Directory.Contact.Services;
 using Xunit;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Directory.Data;
 
 namespace Directory.xUnitTest
 {
@@ -33,6 +36,35 @@ namespace Directory.xUnitTest
             var vResult = await _contactService.AddContact(vPerson);
             Assert.True(vResult.Success);
         }
-        
+
+        [Fact]
+        public async void GetContactSummary_ReturnContactSummary_Type()
+        {
+            var vResult = await _contactService.GetContactSummary();
+            Assert.IsType<List<ContactSummary>>(vResult.Payload);
+        }
+
+        [Theory]
+        [InlineData(9999)]
+        public async void GetContactSummaryById_ReturnContactSummary_True(int id)
+        {
+            var vResult = await _contactService.GetContactSummaryById(id);
+            Assert.Null(vResult.Payload);
+        }
+
+        [Fact]
+        public async void RequestReport_ReturnSucces_True()
+        {
+            var vResult = await _contactService.RequestReport();
+            Assert.True(vResult.Success);
+        }
+
+        [Fact]
+        public async void GetReportSummary_ReturnReportSummary_True()
+        {
+            var vResult = await _contactService.ReportSummary();
+            Assert.True(vResult.Success);
+        }
+
     }
 }
