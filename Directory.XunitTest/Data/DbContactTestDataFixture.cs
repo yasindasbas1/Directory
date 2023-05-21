151b9cf1afb235dd05426c44254fbb5f16474052
yasindasbas1 <yasin.dasbas1@gmail.com>
2023-05-21 23:16:55 +0300

    Brim testler için proje oluşturuldu contact servisi test için fixture data oluşturuldu.

---
+++ Directory.XunitTest/Data/DbContactTestDataFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Directory.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Formes.XunitTest.Data
+{
+    public class DbContactTestDataFixture : IDisposable
+    {
+        public ContactContextDb Context { get; private set; }
+
+        public DbContactTestDataFixture()
+        {
+            var options = new DbContextOptionsBuilder<ContactContextDb>()
+                .EnableSensitiveDataLogging()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).Options;            
+                      
+            Context= new ContactContextDb(options);
+
+            Context.SaveChanges();
+            Context.ChangeTracker.Clear();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
